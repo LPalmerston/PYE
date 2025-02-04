44fe0b0e12685b48d627e5b8732bb885590ae28d
NB-Debian <agustin.sartori93@gmail.com>
2025-02-04 13:08:42 -0300

    Primer commit

---
+++ Example.py
@@ -0,0 +1,24 @@
+'''
+print("Hello World")
+
+
+print(a)
+print(a.upper())
+print(a.lower())
+
+a = "Hello World"
+
+b = a.lower()
+
+c = input()
+
+print(b.capitalize(), c)'''
+
+list_a = ["Manzana", "Banana", "Mandarina", "Uvas"]
+
+x = 0
+
+
+for i in list_a:
+    print(i)
+
