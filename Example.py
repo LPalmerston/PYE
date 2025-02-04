bbdf88e609627e0375eec4ae90f1585e5bde56a6
NB-Debian <agustin.sartori93@gmail.com>
2025-02-04 13:41:03 -0300

    Se agrego Dictionary, Set n' Tuples

--- Example.py
+++ Example.py
@@ -1,24 +1,21 @@
-'''
+
 print("Hello World")
 
+a = "Happyness"
 
 print(a)
 print(a.upper())
 print(a.lower())
 
-a = "Hello World"
-
+a = "Eres bienvenido"
 b = a.lower()
-
 c = input()
 
-print(b.capitalize(), c)'''
+print(b.capitalize(), c)
 
 list_a = ["Manzana", "Banana", "Mandarina", "Uvas"]
 
 x = 0
 
-
 for i in list_a:
     print(i)
-
